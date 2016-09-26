2520ce7b6a2eaf7829721f42ded5ef4f3035a7f2
Radik <radik131@gmail.com>
2016-09-26 15:27:00 +0300

    find unpaired

---
+++ ConsoleApp/ConsoleApp.Tests/UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int? res = Program.FindUnpaired(new int[] { 1, 2, 2, 3, 3, 4, 5, 5 });
+            Assert.AreEqual(1, res);
+
+            res = Program.FindUnpaired(new int[] { 2, 2, 3, 3, 5, 5 });
+            Assert.AreEqual(null, res);
+
+            res = Program.FindUnpaired(new int[] { 2, 2, 3, 3, 4, 5, 5 });
+            Assert.AreEqual(4, res);
+
+            res = Program.FindUnpaired(new int[] { 2, 2, 3, 3, 4, 4, 5 });
+            Assert.AreEqual(5, res);
+
+            res = Program.FindUnpaired(new int[] { 1, 2 });
+            Assert.AreEqual(1, res);
+
+            res = Program.FindUnpaired(new int[] { 1 });
+            Assert.AreEqual(1, res);
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
+        private static int[] array = new[] {1, 2, 2, 3, 3, 4, 5, 5};
+
+        /// You've got an asc sorted array of ints like 2,2,3,3,4,5,5... Find an element without a pair.
         static void Main(string[] args)
         {
+            int? unpaired = FindUnpaired(array);
+            Console.WriteLine(unpaired);
+
+            Console.Write("Press ENTER...");
+            Console.Read();
+        }
+
+        public static int? FindUnpaired(int[] arr)
+        {
+            if (arr == null)
+            {
+                return null;
+            }
+
+            int? res = null;
+            for (int i = 0; i < arr.Length; i = i + 2)
+            {
+                if (i == arr.Length - 1 && arr.Length%2 != 0)
+                    return arr[i];
+
+                if (arr[i] != arr[i + 1])
+                {
+                    res = arr[i];
+                    break;
+                }
+            }
+
+            return res;
         }
     }
 }
