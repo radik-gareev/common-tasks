7dba276bfdfca8ed888b43b26b6c55e08e4d9cd8
Radik <radik131@gmail.com>
2016-10-22 15:05:04 +0300

    In sorted array find 3 numbers that add up to 0.

--- ConsoleApp.Tests/UnitTest.cs
+++ ConsoleApp.Tests/UnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleApp.Tests
@@ -9,7 +10,39 @@ namespace ConsoleApp.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Program.Method();
+            int[] arr = new[] { 0, 1, 2, 3, 4};
+            int sum = 6;
+            Tuple<int, int> res = Program.FindTwoArrayElements(arr, sum);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(4, res.Item1);
+            Assert.AreEqual(2, res.Item2);
+
+            arr = new[] { -10, 0, 10, 3, 4 };
+            sum = 0;
+            res = Program.FindTwoArrayElements(arr, sum);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(10, res.Item1);
+            Assert.AreEqual(-10, res.Item2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] arr = new[] { -2, -1, 0, 1, 2};
+            int[] res = Program.FindThreeArrayElements(arr);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(3, res.Length);
+            Assert.IsTrue(res.Contains(-2));
+            Assert.IsTrue(res.Contains(0));
+            Assert.IsTrue(res.Contains(2));
+
+            arr = new[] { -10, 0, 10, 3, 4 };
+            res = Program.FindThreeArrayElements(arr);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(3, res.Length);
+            Assert.IsTrue(res.Contains(-10));
+            Assert.IsTrue(res.Contains(0));
+            Assert.IsTrue(res.Contains(10));
         }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,18 +9,52 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// In sorted array find 3 numbers that add up to 0.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            int[] arr = new[] { -2, -1, 0, 1, 2};
+            int[] res = FindThreeArrayElements(arr);
+            Console.WriteLine(string.Join(", ", res));
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static int[] FindThreeArrayElements(int[] arr)
         {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Tuple<int, int> res = FindTwoArrayElements(arr, (-1) * arr[i]);
+                if (res != null)
+                {
+                    int[] array = new[] {res.Item1, res.Item2, arr[i]};
+                    return array;
+                }
+            }
+
+            return null;
+        }
+
+        public static Tuple<int, int> FindTwoArrayElements(int[] arr, int sum)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dict.ContainsKey(arr[i]))
+                {
+                    int item1 = arr[i];
+                    int item2 = arr[dict[arr[i]]];
+                    Tuple<int, int> result = new Tuple<int, int>(item1, item2);
+                    return result;
+                }
+
+                if (!dict.ContainsKey(sum - arr[i]))
+                    dict[sum - arr[i]] = i;
+            }
+
+            return null;
         }
     }
 }
