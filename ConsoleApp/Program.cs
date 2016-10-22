127861877ab365ab07fb4d28dbd0944488b2bbc5
Radik <radik131@gmail.com>
2016-10-22 21:38:46 +0300

    Write a method to replace all spaces in a string with'%20'.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,48 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Write a method to replace all spaces in a string with'%20'.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            string str = "Mr John Smith";
+            string newString = ReplaceSpaces(str);
+            Console.WriteLine(newString);
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static string ReplaceSpaces(string str)
         {
-            
+            int numberOfSpaces = 0;
+            if (string.IsNullOrEmpty(str))
+                return str;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                    numberOfSpaces++;
+            }
+
+            int newStringLength = numberOfSpaces*3 + str.Length - numberOfSpaces;
+            char[] newString = new char[newStringLength];
+            int k = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    newString[k++] = '%';
+                    newString[k++] = '2';
+                    newString[k++] = '0';
+                }
+                else
+                {
+                    newString[k++] = str[i];
+                }
+            }
+
+            return new string(newString);
         }
     }
 }
