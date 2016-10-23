8d33f0332734c249391fb66c3ebef5d54b3e7c7d
Radik <radik131@gmail.com>
2016-10-23 17:18:00 +0300

    Binary search

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,43 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Binary search
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            int[] arr = new[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+            int elem = 10;
+            int index = BinarySearch(arr, elem);
+            Console.WriteLine(index);
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static int BinarySearch(int[] arr, int elementToFind)
         {
-            
+            int left = 0;
+            int right = arr.Length - 1;
+            int mid = (right + left) / 2;
+
+            while (left <= right)
+            {
+                if (arr[mid] == elementToFind)
+                    return mid;
+
+                if (arr[mid] > elementToFind)
+                {
+                    right = mid + 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+                mid = (right + left) / 2;
+            }
+
+            return -1;
         }
     }
 }
