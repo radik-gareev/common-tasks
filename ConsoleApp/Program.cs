03a650aa76fa23aa0f93d9f9e0606ef5fd6c2a73
Radik <radik131@gmail.com>
2016-10-23 15:23:15 +0300

    Given two strings, s1 and s2, write code to check If s2 is a rotation of s1 using only one call to isSubstring

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,36 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Assume you have a method isSubstring which checks if one word is a substring
+        /// of another.Given two strings, s1 and s2, write code to check If s2 is a rotation of s1
+        /// using only one call to isSubstring(e.g., "waterbottle" is a rotation of "erbottlewat").
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            string s1 = "waterbottle";
+            string s2 = "erbottlewat";
+            bool isRotation = IsRotation(s1, s2);
+            Console.WriteLine(isRotation);
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static bool IsRotation(string s1, string s2)
         {
-            
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2) || s1.Length != s2.Length)
+                return false;
+
+            string s1s1 = s1 + s1;
+            return IsSubstring(s1s1, s2);
+        }
+
+        private static bool IsSubstring(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2))
+                return false;
+
+            return s1.IndexOf(s2, StringComparison.Ordinal) >= 0;
         }
     }
 }
