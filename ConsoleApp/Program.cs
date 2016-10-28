2480e7d4d58a9f33a0be12d63a84f8f0b6ba1938
Radik <radik131@gmail.com>
2016-10-28 11:10:13 +0300

    Calculate Fibonacci Series

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,33 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Calculate Fibonacci Series 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            int length = 100;
+            List<double> fibonacci = Fibonacci(length);
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static List<double> Fibonacci(int length)
         {
-            
+            List<double> result = new List<double>();
+            double a = 0;
+            double b = 1;
+            double c = 0;
+
+            for (int i = 2; i <= length; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                result.Add(c);
+            }
+
+            return result;
         }
     }
 }
