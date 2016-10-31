f0ec46fcf09b0f520f66c0f03e774975085e7a9a
Radik <radik131@gmail.com>
2016-10-31 18:12:03 +0300

    Count the number of ways to move from upper left corner of rectangle to bottom right.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,17 +9,23 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Count the number of ways to move from upper left corner of rectangle to bottom right. 
+        /// Moving is possible to the bottom and to the right
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            int[,] matrix = new int[,]
+            {
+                {0,1,2,3 },
+                {0,1,2,3 },
+                {0,1,2,3 }
+            };
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static int Method()
         {
             
         }
