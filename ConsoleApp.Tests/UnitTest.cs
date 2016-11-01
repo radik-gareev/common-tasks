5f573cb6f1ac7f7e90ea807fe0e724bf8900e179
Radik <radik131@gmail.com>
2016-11-01 17:01:19 +0300

    Split coin sequence into two heaps, making the difference minimal.

--- ConsoleApp.Tests/UnitTest.cs
+++ ConsoleApp.Tests/UnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleApp.Tests
@@ -9,7 +10,21 @@ namespace ConsoleApp.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            //Program.Method();
+            int[] arr = new[] {0, 1, 2, 1};
+            Tuple<int[], int[]> result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(0, (result.Item1.Sum() - result.Item2.Sum()));
+
+            arr = new[] {0, 1, 2, 1, 1};
+            result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(1, (result.Item1.Sum() - result.Item2.Sum()));
+
+            arr = new[] { 0, 1, 2, 1, 1, 10 };
+            result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(5, (result.Item1.Sum() - result.Item2.Sum()));
+
+            arr = new[] { 0, 1, 2, 7, 1, 10 };
+            result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(1, (result.Item1.Sum() - result.Item2.Sum()));
         }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,52 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Split coin sequence into two heaps, making the difference minimal.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            int[] arr = new[] {5,7,8,10,20,52,9,10,1};
+            Tuple<int[], int[]> heaps = SplitIntoTwoArrays(arr);
+            Console.WriteLine(string.Join(", ", heaps.Item1));
+            Console.WriteLine(string.Join(", ", heaps.Item2));
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static Tuple<int[], int[]> SplitIntoTwoArrays(int[] arr)
         {
-            
+            if (arr.Length < 3)
+                return null;
+
+            int[] array1 = new int[arr.Length - 1];
+            int[] array2 = new int[arr.Length - 1];
+
+            Array.Sort(arr);
+            int index1 = 0;
+            int index2 = 0;
+
+            int sum1 = arr[arr.Length - 1];
+            int sum2 = arr[arr.Length - 2];
+
+            array1[index1] = sum1;
+            array2[index2] = sum2;
+
+            for (int i = arr.Length - 3; i >= 0; i--)
+            {
+                if (sum1 > sum2)
+                {
+                    array2[++index2] = arr[i];
+                    sum2 += arr[i];
+                }
+                else
+                {
+                    array1[++index1] = arr[i];
+                    sum1 += arr[i];
+                }
+            }
+
+            return new Tuple<int[], int[]>(array1, array2);
         }
     }
 }
