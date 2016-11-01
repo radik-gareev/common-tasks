b627d3b12e59b5ed7bbc1d2ca4a54f537221bf10
Radik <radik131@gmail.com>
2016-11-01 17:28:45 +0300

    Added failing tests :(

--- ConsoleApp.Tests/UnitTest.cs
+++ ConsoleApp.Tests/UnitTest.cs
@@ -25,6 +25,21 @@ namespace ConsoleApp.Tests
             arr = new[] { 0, 1, 2, 7, 1, 10 };
             result = Program.SplitIntoTwoArrays(arr);
             Assert.AreEqual(1, (result.Item1.Sum() - result.Item2.Sum()));
+
+            arr = new[] { 11, 10, 8, 7, 6 };
+            result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(6, (result.Item1.Sum() - result.Item2.Sum()));
+
+            arr = new[] { 3, 1, 1, 2, 2, 1 };
+            result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(0, (result.Item1.Sum() - result.Item2.Sum()));
+
+            // FAILS!!!
+            // Expected: {7, 8} and {4, 5, 6}. Difference is 0.
+            // Actual: {4, 5, 8} and {6, 7}. Difference is 4.
+            arr = new[] { 4, 5, 6, 7, 8 };
+            result = Program.SplitIntoTwoArrays(arr);
+            Assert.AreEqual(0, (result.Item1.Sum() - result.Item2.Sum()));
         }
     }
 }
