97cd3bec3b8fed1c4fd8db3805d1a43a51d71c54
Radik <radik131@gmail.com>
2016-11-02 17:31:51 +0300

    Given two strings A and B, find the longest common substring.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,60 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Given two strings A and B, find the longest common substring.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            string a = "asdgfbcddsgfsd";
+            string b = "dsgbc123";
+            string common = GetLongestCommonSubstring(a, b);
+            Console.WriteLine(common);
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static string GetLongestCommonSubstring(string a, string b)
         {
-            
+            if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
+                return string.Empty;
+
+            string result = string.Empty;
+            string tempResult = string.Empty;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        int indexA = i;
+                        int indexB = j;
+                        while (indexA < a.Length && indexB < b.Length)
+                        {
+                            if (a[indexA] == b[indexB])
+                            {
+                                tempResult += a[indexA];
+                                indexA++;
+                                indexB++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        j = j + tempResult.Length - 1;
+                    }
+                    else
+                    {
+                        if (tempResult.Length > result.Length)
+                            result = tempResult;
+                        tempResult = string.Empty;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
--- ConsoleApp/UnitTests.cs
+++ ConsoleApp/UnitTests.cs
@@ -8,7 +8,50 @@ namespace ConsoleApp
         [TestMethod]
         public void Test()
         {
-            
+            string a = "123456";
+            string b = "123456";
+            string result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("123456", result);
+
+            a = "123456";
+            b = "123";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("123", result);
+
+            a = "123456";
+            b = "456";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("456", result);
+
+            a = "456";
+            b = "123456";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("456", result);
+
+            a = "123";
+            b = "123456";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("123", result);
+
+            a = "123456";
+            b = "2346";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("234", result);
+
+            a = "2346";
+            b = "123456";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("234", result);
+
+            a = "2346";
+            b = "987";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("", result);
+
+            a = "xcv";
+            b = "912387";
+            result = Program.GetLongestCommonSubstring(a, b);
+            Assert.AreEqual("", result);
         }
     }
 }
