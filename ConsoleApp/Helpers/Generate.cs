eb4971a3541666dddab3e1616ad4a8c1daf2b1fd
Radik <radik131@gmail.com>
2016-11-03 13:22:43 +0300

    Added helpers

---
+++ ConsoleApp/Helpers/Generate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Helpers
+{
+    public static class Generate
+    {
+        public static LinkedList LinkedListFromArray(int[] arr)
+        {
+            if (arr == null)
+                return null;
+            LinkedList head = new LinkedList(arr[0]);
+            LinkedList current = head;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                current.Next = new LinkedList(arr[i]);
+                current = current.Next;
+            }
+
+            return head;
+        }
+    }
+}
