b372b5610d8ca20d1cb3f9a64ef1159d157b3e13
Radik <radik131@gmail.com>
2016-11-03 13:03:06 +0300

    Sort single linked list.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,19 +10,41 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Sort single linked list.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            LinkedList node = Generate.LinkedListFromArray(new[] { 2, 4, 9, 1, 2, 0, 3, 72 });
+            Print.LinkedList(node);
+            SortSingleLinkedList(node);
+            Print.LinkedList(node);
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static void SortSingleLinkedList(LinkedList node)
         {
-            
+            LinkedList outer = node;
+            LinkedList inner = node;
+            while (outer != null)
+            {
+                while (inner != null)
+                {                                    
+                    if (inner.Next != null && inner.Value > inner.Next.Value)
+                    {
+                        int temp = inner.Value;
+                        inner.Value = inner.Next.Value;
+                        inner.Next.Value = temp;
+                    }
+
+                    inner = inner.Next;
+                }
+
+                outer = outer.Next;
+                inner = node;
+            }
         }
     }
 }
