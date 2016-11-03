8e9597f2b880f9259255b3fed94fbfd21ae0f731
Radik <radik131@gmail.com>
2016-11-03 10:40:56 +0300

    Sort an array with length N where 0 <= a[i] < N

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,19 +9,37 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Sort an array with length N where 0 <= a[i] < N
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            int[] arr = new[] { 0, 4, 5, 5, 4, 2, 1, 2 };
+            CountingSort(arr);
+            Console.WriteLine(string.Join(", ", arr));
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static int[] CountingSort(int[] arr)
         {
-            
+            int[] counting = new int[arr.Length];
+            int[] sortedArray = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                counting[arr[i]]++;
+            }
+
+            int sortedIndex = 0;
+            for (int i = 0; i < counting.Length; i++)
+            {
+                for (int j = 0; j < counting[i]; j++)
+                {
+                    arr[sortedIndex++] = i;
+                }
+            }
+
+            return sortedArray;
         }
     }
 }
