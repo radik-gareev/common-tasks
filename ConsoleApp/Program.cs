f887da664b5c27c09b7a1976d93947b0b510633d
Radik <radik131@gmail.com>
2016-11-07 12:07:46 +0300

    Find the number of possible combinations of building tower of length X using cups.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,20 +9,52 @@ namespace ConsoleApp
 {
     public class Program
     {
+        private static int _towerHeight = 21;
+        private static List<string> combinations = new List<string>();
+
+
         /// <summary>
-        /// 
+        /// You have cups of height 10. You can put it one upon another and get tower of length 20 or 11 depending on cup orientation. 
+        /// Find the number of possible combinations of building tower of length X using cups. 
+        /// Two combinations are equal if Nth cup is oriented equally in both combinations for each N meaning count of cup from bottom
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            if (_towerHeight >= 10)
+            {
+                GetNumberOfCombinations(10, 0, string.Empty);
+                GetNumberOfCombinations(10, 1, string.Empty);
+            }
+
+            bool isUnique = combinations.Distinct().Count() == combinations.Count();
+            for (int i = 0; i < combinations.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ": " + combinations[i]);
+            }
+
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        /// <returns></returns>
+        public static void GetNumberOfCombinations(int previousHeight, byte previousOrientation, string combination)
         {
-            
+            combination += previousOrientation + " ";
+            if (previousHeight > _towerHeight)
+            {
+                return;
+            }
+
+            if (previousHeight == _towerHeight)
+            {
+                combinations.Add(combination);
+            }
+            else
+            {
+                GetNumberOfCombinations(previousHeight + 1, previousOrientation, combination);
+                GetNumberOfCombinations(previousHeight + 10, (byte) (previousOrientation ^ 1), combination);
+            }
         }
     }
 }
