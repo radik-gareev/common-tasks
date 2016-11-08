9c37e975e2d6cf9d24c2566068c424a279f39200
Radik <radik131@gmail.com>
2016-11-08 13:08:20 +0300

    Tree helper

--- ConsoleApp/Helpers/Tree.cs
+++ ConsoleApp/Helpers/Tree.cs
@@ -11,10 +11,27 @@ namespace ConsoleApp.Helpers
         public Tree Left;
         public Tree Right;
         public int Value;
+        public string DfsTraversal;
 
         public Tree(int value)
         {
             Value = value;
+        }
+
+
+        public static Tree GetSampleTree()
+        {
+            Tree tree = new Tree(5,
+                new Tree(3,
+                    new Tree(1,
+                        new Tree(0), new Tree(2)),
+                    new Tree(4)),
+                new Tree(7,
+                    new Tree(6),
+                    new Tree(8, null,
+                        new Tree(9))));
+
+            return tree;
         }
 
         public Tree(int value, Tree left, Tree right)
@@ -22,6 +39,36 @@ namespace ConsoleApp.Helpers
             Value = value;
             Left = left;
             Right = right;
+        }
+
+        public string BFS(Tree root)
+        {
+            Queue<Tree> queue = new Queue<Tree>();
+            queue.Enqueue(root);
+            string result = "";
+            while (queue.Any())
+            {
+                Tree node = queue.Dequeue();
+                result += node.Value + " ";
+
+                if (node.Left != null)
+                    queue.Enqueue(node.Left);
+
+                if (node.Right != null)
+                    queue.Enqueue(node.Right);
+            }
+
+            return result;
+        }
+
+        public void DFS(Tree node)
+        {
+            if (node == null)
+                return;
+
+            DfsTraversal += node.Value + " ";
+            DFS(node.Left);
+            DFS(node.Right);
         }
     }
 }
