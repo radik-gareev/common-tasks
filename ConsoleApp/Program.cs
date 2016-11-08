4de1d7f17589c14fa828c1f07305893acd683560
Radik <radik131@gmail.com>
2016-11-08 12:43:56 +0300

    Construct sorted linked list from binary search tree

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,11 +10,20 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Construct sorted linked list from binary search tree
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Tree tree = new Tree(5, 
+                            new Tree(3, 
+                                new Tree(1, 
+                                    new Tree(0), new Tree(2)), 
+                                new Tree(4)), 
+                            new Tree(7, 
+                                new Tree(6), 
+                                new Tree(8, null, 
+                                    new Tree(9))));
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
@@ -23,6 +32,16 @@ namespace ConsoleApp
         public static void Method()
         {
             
+        }
+
+        private static string DFS(Tree node, string result)
+        {
+            if (node == null)
+                return string.Empty;
+
+            result += node.Value + " ";
+            DFS(node.Left, result);
+            DFS(node.Right, result);
         }
     }
 }
