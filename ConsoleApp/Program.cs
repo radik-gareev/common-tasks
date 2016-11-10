4265527223733d50fc66768fe81fd86c4d17c0d1
Radik <radik131@gmail.com>
2016-11-10 18:10:29 +0300

    Inital try of O(n*log(n))  algorithm

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,9 +15,10 @@ namespace ConsoleApp
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int[] arr = new[] { 2, 5, 1, 3, 4 };
+            //int[] arr = new[] { 2, 5, 1, 3, 4 };
+            int[] arr = new[] { 1, 2, 3, 4, 5, 6 };
 
-            int[] range = GetMaxOrRange(arr);
+            int[] range = GetMaxOrRange_Improved(arr);
 
             Console.WriteLine("Range: " + string.Join(", ", range));
             Console.Write("Press ENTER...");
@@ -26,7 +27,7 @@ namespace ConsoleApp
 
         public static int[] GetMaxOrRange(int[] arr)
         {
-            if(arr == null || arr.Length < 2)
+            if (arr == null || arr.Length < 2)
                 return new int[0];
 
             int totalOr = 0;
@@ -72,5 +73,87 @@ namespace ConsoleApp
 
             return new int[0];
         }
+
+        public static int[] GetMaxOrRange_Improved(int[] arr)
+        {
+            if (arr == null || arr.Length < 2)
+                return new int[0];
+
+            int totalOr = 0;
+            int startIndex = 0;
+            int endIndex = arr.Length - 1;
+            int prevRangeLength = arr.Length;
+
+            for (int i = 0; i < arr.Length; i++)
+                totalOr = totalOr | arr[i];
+
+            int[] suffixSum = new int[arr.Length];
+            suffixSum[0] = 0;
+            for (int i = 1; i < suffixSum.Length; i++)
+            {
+                suffixSum[i] = suffixSum[i - 1] | arr[i - 1];
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int left = i + 1;
+                int right = suffixSum.Length - 1;
+                int mid = (right + left) / 2;
+
+                while (left <= right)
+                {
+                    if ((suffixSum[mid] | arr[i]) == totalOr)
+                    {
+                        if (mid - i < prevRangeLength)
+                        {
+                            startIndex = i;
+                            endIndex = mid;
+                            prevRangeLength = endIndex - startIndex;
+                        }
+
+                        break;
+                    }
+
+                    if ((suffixSum[mid] | arr[i]) < totalOr)
+                    {
+                        left = mid + 1;
+                    }
+
+                    mid = (right + left) / 2;
+                }
+
+            }
+
+            Console.WriteLine("Total OR: " + totalOr);
+            if (endIndex >= startIndex)
+            {
+                int[] result = new int[endIndex - startIndex + 1];
+                Array.Copy(arr, startIndex, result, 0, result.Length);
+
+                return result;
+            }
+
+            return new int[0];
+        }
+
+
+        public static int GetMaxSum(int[] arr)
+        {
+
+            int totalMaxSum = 0;
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+                if (sum < 0)
+                    sum = 0;
+                if (totalMaxSum < sum)
+                    totalMaxSum = sum;
+            }
+
+            return totalMaxSum;
+        }
+
     }
 }
