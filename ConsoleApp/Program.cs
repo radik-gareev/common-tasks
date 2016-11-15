6898e25df5ea912b1fa428a9b5c30a5a218877ae
Radik <radik131@gmail.com>
2016-11-15 12:38:55 +0300

    Please find number of way frog could jump from 0 cell to N with given jump sizes array.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,20 +9,54 @@ namespace ConsoleApp
 {
     public class Program
     {
+        private static Dictionary<int, int> positionsToNumberOfCombinations = new Dictionary<int, int>();
+
         /// <summary>
-        /// 
+        /// Please find number of way frog could jump from 0 cell to N with given jump sizes array.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] jumps = new int[] { 1, 2 };
+
+            int count = GetNumberOfJumpsCombinations(array, jumps);
+            Console.WriteLine(count);
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static int GetNumberOfJumpsCombinations(int[] array, int[] jumps)
         {
-            
+            int count = FindNumberOfWays_Recursive(array.Length, jumps);
+            return count;
+        }
+
+        private static int FindNumberOfWays_Recursive(int remainingPositions, int[] jumps)
+        {
+            if (remainingPositions == 0)
+            {
+                return 1;
+            }
+
+            if (remainingPositions < 0)
+            {
+                return 0;
+            }
+
+            if (positionsToNumberOfCombinations.ContainsKey(remainingPositions))
+                return positionsToNumberOfCombinations[remainingPositions];
+
+            int count = 0;
+
+            for (int i = 0; i < jumps.Length; i++)
+            {
+                count += FindNumberOfWays_Recursive(remainingPositions - jumps[i], jumps);
+            }
+
+            positionsToNumberOfCombinations.Add(remainingPositions, count);
+
+            return count;
         }
     }
 }
--- ConsoleApp/UnitTests.cs
+++ ConsoleApp/UnitTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ConsoleApp.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleApp
 {
@@ -6,9 +10,29 @@ namespace ConsoleApp
     public class UnitTests
     {
         [TestMethod]
-        public void Test()
+        public void Comparison_GetNumberOfJumpCombinations()
         {
-            
+            List<Tuple<int[], int[]>> randomHeights = new List<Tuple<int[], int[]>>();
+            int testCases = 1000;
+            for (int i = 0; i < testCases; i++)
+            {
+                int[] randomArray = Generator.CreateRandomArrayWithRandomLength(50, 100);
+                int[] randomJumps = Generator.CreateRandomArrayWithRandomLength(10, 50, 2, 10);
+                randomHeights.Add(new Tuple<int[], int[]>(randomArray, randomJumps));
+            }
+
+            for (int i = 0; i < randomHeights.Count; i++)
+            {
+                int[] randomArray = randomHeights[i].Item1;
+                int[] randomJumps = randomHeights[i].Item2;
+
+                Debug.WriteLine("{0}. Testing with randomArray.Length={1} and randomJumps.Length={2}...", i + 1, randomArray.Length, randomJumps.Length);
+
+                var stopwatch = Stopwatch.StartNew();
+                int count = Program.GetNumberOfJumpsCombinations(randomArray, randomJumps);
+                stopwatch.Stop();
+                Debug.WriteLine("{0}. GetNumberOfJumpsCombinations_Improved finished in {1}ms, result={2}", i + 1, stopwatch.ElapsedMilliseconds, count);
+            }
         }
     }
 }
