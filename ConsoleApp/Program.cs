aa2825f24f17b1ebd53e612b177a9c2fa272b80b
Radik <radik131@gmail.com>
2016-11-16 15:23:30 +0300

    Ugly solution but it looks working

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,19 +10,56 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Restore valid brackets sequence by adding necessary count of '(' and ')'
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            string str = "((((()(((";
+
+            Console.WriteLine(str);
+            Console.WriteLine(BuildValidBracketsSequence(str));
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static string BuildValidBracketsSequence(string str)
         {
-            
+            int balance = 0;
+            for (int i = 1; i < str.Length - 1 && str.Length > 2; i++)
+            {
+                if (str[i] == '(')
+                    balance++;
+                else if (str[i] == ')')
+                    balance--;
+            }
+
+            // check first and last bracket
+            if (str[0] == ')')
+                str = '(' + str;
+            else
+                balance++;
+
+            if (str[str.Length - 1] == '(')
+                str = str + ')';
+            else
+                balance--;
+
+
+
+            // need to add '('
+            if (balance < 0)
+            {
+                str = new string('(', -1*balance) + str; 
+            }
+
+            // need to add ')'
+            if (balance > 0)
+            {
+                str = str + new string(')', balance);
+            }
+
+            return str;
         }
     }
 }
--- ConsoleApp/UnitTests.cs
+++ ConsoleApp/UnitTests.cs
@@ -12,26 +12,6 @@ namespace ConsoleApp
         [TestMethod]
         public void Test()
         {
-
-            List<int[]> randomArrays = new List<int[]>();
-            int testCases = 100;
-            for (int i = 0; i < testCases; i++)
-            {
-                int[] array = Generator.ArrayWithRandomLength(50, 100);
-                randomArrays.Add(array);
-            }
-
-            for (int i = 0; i < randomArrays.Count; i++)
-            {
-                int[] randomArray = randomArrays[i];
-
-                Debug.WriteLine("{0}. Testing with randomArray.Length={1}...", i + 1, randomArray.Length);
-
-                var stopwatch = Stopwatch.StartNew();
-                Program.Method();
-                stopwatch.Stop();
-                Debug.WriteLine("{0}. GetNumberOfJumpsCombinations_Improved finished in {1}ms, result={2}", i + 1, stopwatch.ElapsedMilliseconds, "");
-            }
         }
 
     }
