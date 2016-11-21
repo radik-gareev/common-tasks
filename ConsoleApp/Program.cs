a6d23201f4da30e529f4f8fac66bdf8eca1a3688
Radik <radik131@gmail.com>
2016-11-21 17:05:37 +0300

    Reverse words in array without additional memory

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,19 +10,35 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Reverse words in array without additional memory
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            int[] arr = new[] { 0, 1, 2, 3, 4, 5};
+            Console.WriteLine(string.Join(",", ReverseArray(arr)));
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static void Method()
+        public static int[] ReverseArray(int[] arr)
         {
-            
+            if (arr == null || arr.Length < 2)
+                return arr;
+
+            int i = 0;
+            int n = arr.Length;
+            while (i < n / 2)
+            {
+                int a = arr[i];
+                int b = arr[n - i - 1];
+                int tmp = a;
+                arr[i] = b;
+                arr[n - i - 1] = tmp;
+                i++;
+            }
+
+            return arr;
         }
     }
 }
