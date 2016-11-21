03078aa715f6d16fdb1c0c878dd4acea63790747
Radik <radik131@gmail.com>
2016-11-21 15:32:14 +0300

    Added search for the longest line with two zeros.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -22,40 +22,50 @@ namespace ConsoleApp
         {
             int[,] arr = new int[,]
             {
-                { 0,1,1,0,1},
+                //{ 0,1,0,1,0},
                 //{ 0,1,0,1,1,0},
-                //{ 1,0,0,0,0,1},
+                { 1,0,1,1,0,0},
                 //{ 1,1,1,0,0,0},
                 //{ 0,1,0,1,0,1},
                 //{ 1,0,1,0,0,1},
                 //{ 0,1,1,0,1,0},
             };
-            Console.WriteLine(GetLongestLineLength(arr));
+            Console.WriteLine("Line with 1 zero:  " + GetLongestLineLength(arr, zerosCount: 1));
+            Console.WriteLine("Line with 2 zeros: " + GetLongestLineLength(arr, zerosCount: 2));
             Console.Write("Press ENTER...");
             Console.Read();
         }
 
-        public static int GetLongestLineLength(int[,] arr)
+        public static int GetLongestLineLength(int[,] arr, byte zerosCount)
         {
             int rowMaxLength = 0;
 
             // search in rows
             for (int i = 0; i < arr.GetLength(0); i++)
             {
-                rowMaxLength = Math.Max(rowMaxLength, GetRowMaxLength(arr, i));
+                if (zerosCount == 1)
+                    rowMaxLength = Math.Max(rowMaxLength, GetRowMaxLengthOneZero(arr, i));
+                else if (zerosCount == 2)
+                    rowMaxLength = Math.Max(rowMaxLength, GetRowMaxLengthTwoZeros(arr, i));
+
             }
 
             // search in columns
             int colMaxLength = 0;
             for (int i = 0; i < arr.GetLength(1); i++)
             {
-                colMaxLength = Math.Max(colMaxLength, GetColMaxLength(arr, i));
+                if (zerosCount == 1)
+                    colMaxLength = Math.Max(colMaxLength, GetColMaxLengthOneZero(arr, i));
+                else if (zerosCount == 2)
+                    colMaxLength = Math.Max(colMaxLength, GetColMaxLengthTwoZeros(arr, i));
             }
 
             return Math.Max(rowMaxLength, colMaxLength);
         }
 
-        private static int GetRowMaxLength(int[,] arr, int row)
+        #region search for one zero max sequence
+
+        private static int GetRowMaxLengthOneZero(int[,] arr, int row)
         {
             int maxLength = 0;
             int prevZeroIndex = -1;
@@ -81,7 +91,7 @@ namespace ConsoleApp
             return maxLength;
         }
 
-        private static int GetColMaxLength(int[,] arr, int col)
+        private static int GetColMaxLengthOneZero(int[,] arr, int col)
         {
             int maxLength = 0;
             int prevZeroIndex = -1;
@@ -107,6 +117,65 @@ namespace ConsoleApp
             return maxLength;
         }
 
+        #endregion
+
+        #region search for two zeros max sequence
+
+        private static int GetRowMaxLengthTwoZeros(int[,] arr, int row)
+        {
+            int maxLength = 0;
+            int prevZeroIndex = -1;
+            int prevPrevZeroIndex = -1;
+            int prevPrevPrevZeroIndex = -1;
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[row, j] == 0)
+                {
+                    if (j - prevPrevPrevZeroIndex - 1 > maxLength)
+                    {
+                        maxLength = j - prevPrevPrevZeroIndex - 1;
+                    }
+
+                    prevPrevPrevZeroIndex = prevPrevZeroIndex;
+                    prevPrevZeroIndex = prevZeroIndex;
+                    prevZeroIndex = j;
+                }
+            }
+
+            if (arr.GetLength(1) - prevPrevPrevZeroIndex - 1 > maxLength)
+            {
+                maxLength = arr.GetLength(1) - prevPrevPrevZeroIndex - 1;
+            }
+            return maxLength;
+        }
+
+        private static int GetColMaxLengthTwoZeros(int[,] arr, int col)
+        {
+            int maxLength = 0;
+            int prevZeroIndex = -1;
+            int prevPrevZeroIndex = -1;
+            for (int j = 0; j < arr.GetLength(0); j++)
+            {
+                if (arr[j, col] == 1)
+                {
+                    if (j - prevPrevZeroIndex - 1 > maxLength)
+                    {
+                        maxLength = j - prevPrevZeroIndex - 1;
+                    }
+
+                    prevPrevZeroIndex = prevZeroIndex;
+                    prevZeroIndex = j;
+                }
+            }
+
+            if (arr.GetLength(0) - prevPrevZeroIndex - 1 > maxLength)
+            {
+                maxLength = arr.GetLength(0) - prevPrevZeroIndex - 1;
+            }
+            return maxLength;
+        }
+
+        #endregion
     }
 
 
