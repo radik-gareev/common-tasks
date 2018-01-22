a7aa359ba8f2af90ae21243f637b73dea1d5eb3b
Radik <radik131@gmail.com>
2018-01-22 15:39:20 +0300

    Write code to remove duplicates from an unsorted linked list.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,14 +23,17 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Write code to remove duplicates from an unsorted linked list.
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-                
+                LinkedList list = Generator.LinkedListFromArray(new[] {1, 2, 5, 4, 6, 2, 5});
+                Print.LinkedList(list);
+                RemoveDuplicates(list);
+                Print.LinkedList(list);
             }
             catch (Exception e)
             {
@@ -40,6 +43,30 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static void RemoveDuplicates(LinkedList head)
+        {
+            LinkedList current = head;
+
+            while (current != null)
+            {
+                LinkedList inner = current;
+
+                while (inner.Next != null)
+                {
+                    if (current.Value == inner.Next.Value)
+                    {
+                        inner.Next = inner.Next.Next;
+                    }
+                    else
+                    {
+                        inner = inner.Next;
+                    }
+                }
+
+                current = current.Next;
+            }
         }
     }
 }
