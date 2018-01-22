8adab834adaadbb111e48724a1c8a74d5849227d
Radik <radik131@gmail.com>
2018-01-22 16:07:37 +0300

    Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,14 +23,24 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
-        /// </summary>
+        /// Implement an algorithm to delete a node in the middle of a singly linked list,
+        /// given only access to that node.
+        /// EXAMPLE
+        /// Input: the node c from the linked list a->b->c->d->e
+        /// Result: nothing is returned, but the new linked list looks like a- >b- >d->e        /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-                
+                LinkedList list = Generator.LinkedListFromArray(new[] { 1, 2, 5, 4 });
+                Print.LinkedList(list);
+
+                int toDelete = 5;
+
+                DeleteNode(list, toDelete);
+
+                Print.LinkedList(list);
             }
             catch (Exception e)
             {
@@ -40,6 +50,30 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static void DeleteNode(LinkedList head, int toDelete)
+        {
+            LinkedList node = head;
+
+            while (node.Value != toDelete)
+            {
+                node = node.Next;
+            }
+
+            node.Value = node.Next.Value;
+            node.Next = node.Next.Next;
+
+            //while (node != null)
+            //{
+            //    node.Value = node.Next.Value;
+            //    if (node.Next.Next == null)
+            //    {
+            //        node.Next = null;
+            //    }
+
+            //    node = node.Next;
+            //}
         }
     }
 }
