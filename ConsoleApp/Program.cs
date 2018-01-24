bb608d9ce111a8d9766ba96eca6eea3a6ba8b007
Radik <radik131@gmail.com>
2018-01-24 15:58:03 +0300

    find the English pronunciation of the given number, e.g. 2012 - two thousand and twelve

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,14 +23,16 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// find the English pronunciation of the given number, e.g. 2012 - two thousand and twelve
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-                
+                int num = 91981;
+
+                string res = IntToEnglish(num);
             }
             catch (Exception e)
             {
@@ -40,6 +42,112 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static string[] decimals = new[]
+        {
+            "",
+            "",
+            "twenty",
+            "thirty",
+            "forty",
+            "feefty",
+            "sixty",
+            "seventy",
+            "eighty",
+            "ninety",
+        };
+
+        private static string[] teens = new[]
+        {
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+        };
+
+        private static string[] ones = new[]
+        {
+            "",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+        };
+
+        private static string IntToEnglish(int num)
+        {
+            string result = "";
+            int billions = num / 1000000000;
+
+            if (billions > 0)
+            {
+                num = num%1000000000;
+                result = IntToEnglish(billions) + " billions " + IntToEnglish(num);
+                return result;
+            }
+
+            int millions = num/1000000;
+
+            if (millions > 0)
+            {
+                num = num % 1000000;
+                result = IntToEnglish(millions) + " millions " + IntToEnglish(num);
+                return result;
+            }
+
+            int thousands = num / 1000;
+
+            if (thousands > 0)
+            {
+                num = num % 1000;
+                result = IntToEnglish(thousands) + " thousands " + IntToEnglish(num);
+                return result;
+            }
+
+            int hundreds = num / 100;
+
+            if (hundreds > 0)
+            {
+                num = num % 100;
+                result = IntToEnglish(hundreds) + " hundreds " + IntToEnglish(num);
+                return result;
+            }
+
+            int decs = num / 10;
+
+            if (decs > 0)
+            {
+                num = num % 10;
+                if (decs == 1)
+                {
+                    result += teens[num];
+
+                    return result;
+                }
+                else
+                {
+                    result += decimals[decs] + " "+ ones[num];
+
+                    return result;
+                }
+            }
+
+            result = ones[num];
+
+            return result;
         }
     }
 }
