35d98d163fd0b1f940b4eefdcdb952936b5f10e6
Radik <radik131@gmail.com>
2018-01-25 11:19:38 +0300

    Stone Division, Revisited https://www.hackerrank.com/challenges/stone-division-2/problem

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,14 +23,15 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Stone Division, Revisited
+        /// https://www.hackerrank.com/challenges/stone-division-2/problem
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-                
+                var res = stoneDivision(64, new long[] { 2, 4, 8, 16, 32, 64 });
             }
             catch (Exception e)
             {
@@ -40,6 +41,31 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        static Dictionary<long, long> dict = new Dictionary<long, long>(); 
+
+        static long stoneDivision(long n, long[] s)
+        {
+            if (dict.ContainsKey(n))
+                return dict[n];
+
+            long result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] < n && n % s[i] == 0)
+                {
+                    long temp = (n / s[i]) * stoneDivision(s[i], s) + 1;
+                    if (temp > result)
+                    {
+                        result = temp;
+                        dict[n] = result;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
