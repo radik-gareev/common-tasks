57be47d4047143e07f960976a49b112e01321702
Radik <radik131@gmail.com>
2018-01-29 12:32:39 +0300

    Write a method to encode the binary tree to an array of strings and to decode this array back to the tree maintaining the initial tree structure

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,23 +23,87 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// Write a method to encode the binary tree to an array of strings and
+        /// to decode this array back to the tree maintaining the initial tree structure
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-                
+                Tree tree1 = new Tree(1,
+                        new Tree(2,
+                            new Tree(3,
+                                new Tree(4),
+                                new Tree(5)),
+                            new Tree(6)),
+                        new Tree(7,
+                            new Tree(8),
+                            new Tree(9,
+                                new Tree(10),
+                                null)));
+
+                Tree tree2 = new Tree(1,
+                        new Tree(2,
+                            new Tree(3,
+                                new Tree(4),
+                                null),
+                            new Tree(5)),
+                        new Tree(6,
+                            new Tree(7),
+                            new Tree(8)));
+
+                List<string> res = SerializeTree(tree2);
+                Tree deserializedTree = DeserializeTree(res.ToArray());
             }
             catch (Exception e)
             {
-                throw e;
+                throw;
             }
 
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static int index = 0;
+        private static Tree DeserializeTree(string[] s)
+        {
+            if (index == s.Length || s[index] == "#")
+            {
+                index++;
+                return null;
+            }
+
+            int value = Convert.ToInt32(s[index++]);
+            Tree tree = new Tree(value);
+            tree.Left = DeserializeTree(s);
+            tree.Right = DeserializeTree(s);
+
+            return tree;
+        }
+
+        private static List<string> SerializeTree(Tree tree)
+        {
+            List<string> result = new List<string>();
+
+            TraverseDfs(tree, result);
+
+            return result;
+        }
+
+        private static void TraverseDfs(Tree tree, List<string> s)
+        {
+            if (tree == null)
+            {
+                s.Add("#");
+                return;
+            }
+
+            s.Add(tree.Value.ToString());
+
+            TraverseDfs(tree.Left, s);
+            TraverseDfs(tree.Right, s);
         }
     }
 }
