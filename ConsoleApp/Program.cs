ff45e0f0ec8508855d066fb181d569d6766cab4e
Radik <radik131@gmail.com>
2018-06-13 22:42:41 -0700

    Convert to .NET Core

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,14 +8,8 @@ using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleApp.Helpers;
-using System.Management.Automation;
-using System.Management.Automation.Runspaces;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Auth;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.Deployment.WindowsInstaller;
 using Microsoft.Win32;
 
 namespace ConsoleApp
@@ -30,7 +24,7 @@ namespace ConsoleApp
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
