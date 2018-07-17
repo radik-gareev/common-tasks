6247d24cb86190180832cc44b176e000af002231
Radik <radik131@gmail.com>
2018-07-16 21:36:05 -0700

    1.1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,14 +17,17 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// 1.1 Implement an algorithm to determine if a string has all unique characters. What
+        /// if you cannot use additional data structures?
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-
+                string s = Generator.StringRandomLength(20, 280);
+                //string s = "aba";
+                bool result = AllUniqueChars2(s);
             }
             catch (Exception e)
             {
@@ -34,6 +37,38 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static bool AllUniqueChars(string s)
+        {
+            HashSet<char> dict = new HashSet<char>();
+
+            foreach(char c in s)
+            {
+                if (dict.Contains(c))
+                    return false;
+                else
+                    dict.Add(c);
+            }
+
+            return true;
+        }
+
+        private static bool AllUniqueChars2(string s)
+        {
+            if (s.Length > 256)
+                return false;
+
+            bool[] chars = new bool[256];
+            foreach(char c in s)
+            {
+                int k = Convert.ToInt32(c);
+                if (chars[k])
+                    return false;
+                else chars[k] = true;
+            }
+
+            return true;
         }
     }
 }
