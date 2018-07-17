df61557536f4cf877f00bf7a50ca1e90e7721c98
Radik <radik131@gmail.com>
2018-07-16 21:49:33 -0700

    1.2 Implement a function void reverse(char* str) in C or C++ which reverses a nullterminated string.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,14 +17,15 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// 1.2 Implement a function void reverse(char* str) in C or C++ which reverses a nullterminated string.
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
             try
             {
-
+                string s = Generator.StringRandomLength(2, 10);
+                string reversed = Reverse2(s);
             }
             catch (Exception e)
             {
@@ -34,6 +35,41 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static string Reverse(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+                return String.Empty;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i=s.Length - 1;i>=0;i--)
+            {
+                sb.Append(s[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        private static string Reverse2(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return String.Empty;
+
+            int start = 0;
+            int end = s.Length-1;
+
+            char[] chars = s.ToCharArray();
+
+            while (start < end)
+            {
+                char tmp = chars[start];
+                chars[start++] = chars[end];
+                chars[end--] = tmp;
+            }
+
+            return new string(chars);
         }
     }
 }
