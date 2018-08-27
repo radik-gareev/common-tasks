e3af257b7d6118ed9217530ee414ff20142152ed
Radik <radik131@gmail.com>
2018-08-26 23:05:39 -0700

    1.5 Implement a method to perform basic string compression

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,23 +17,50 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// 1.5 Implement a method to perform basic string compression using the counts
+        /// of repeated characters.For example, the string aabcccccaaa would become
+        /// a2blc5a3. If the "compressed" string would not become smaller than the original
+        /// string, your method should return the original string.
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
-            try
-            {
-
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            string res = Compress("aabcccccaaa");
 
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static string Compress(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 1)
+                return s;
+
+            string result =string.Empty;
+            char prev = s[0];
+            int count = 1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (prev == s[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    result += prev.ToString() + count;
+                    count = 1;
+                    prev = s[i];
+                }
+            }
+
+            result += prev.ToString() + count;
+
+            if (result.Length >= s.Length)
+                return s;
+
+            return result;
         }
     }
 }
