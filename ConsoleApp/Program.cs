71699aa0bba766886ba76b8a6d99821ecbb13847
Radik <radik131@gmail.com>
2019-01-17 17:00:27 -0800

    4.5 Implement a function to check if a binary tree is a binary search tree

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,14 +17,28 @@ namespace ConsoleApp
     public class Program
     {
         /// <summary>
-        /// 
+        /// 4.5 Implement a function to check if a binary tree is a binary search tree
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
+            bool res = IsBST(Generator.SampleTree());
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        private static bool IsBST(Tree root, int minValue = int.MinValue, int maxValue = int.MaxValue)
+        {
+            if (root == null)
+                return true;
+
+            if (root.Value < minValue || root.Value > maxValue)
+            {
+                return false;
+            }
+
+            return IsBST(root.Left, minValue, root.Value) && IsBST(root.Right, root.Value, maxValue);
         }
     }
 }
