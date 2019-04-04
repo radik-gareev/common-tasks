9fc892f152fdfe0bd1710f3b8901606995536e82
Radik <radik131@gmail.com>
2019-04-03 23:45:18 -0700

    Given an M x N matrix in which each row and each column is sorted in ascending order, write a method to find an element.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,30 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-using ConsoleApp.Helpers;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.Win32;
 
 namespace ConsoleApp
 {
     public class Program
     {
         /// <summary>
-        /// 
+        /// Given an M x N matrix in which each row and each column is sorted in ascending
+        /// order, write a method to find an element.
         /// </summary>
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
+            int[,] m = new int[,]
+            {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+            };
+
+            bool res = SearchMatrix(m, 45);
             Console.WriteLine();
             Console.Write("Press ENTER...");
             Console.Read();
+        }
+
+        public static bool SearchMatrix(int[,] m, int target)
+        {
+            int col = 0;
+            int row = m.GetLength(1) - 1;
+
+            while (col < m.GetLength(0) && row >= 0)
+            {
+                if (target == m[col, row]) return true;
+                if (target < m[col, row])
+                {
+                    row--;
+                }
+                else
+                {
+                    col++;
+                }
+            }
+
+            return false;
         }
     }
 }
